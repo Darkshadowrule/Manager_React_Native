de43985b6a8bf3affb2978506080e70d1daf2d67
darkshadowrule <prajjwaldxi3@gmail.com>
2019-05-18 23:26:51 +0530

    Created a Redux Based Login form with firebase

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import{createStore,applyMiddleware} from 'redux'
+import reducers from './src/reducers'
+import firebase from 'firebase';
+import LoginForm from './src/components/LoginForm'; 
+import ReduxThunk from 'redux-thunk'  ;
+ class App extends React.Component {
+   componentWillMount()
+   {
+   const firebaseConfig = {
+      apiKey: "AIzaSyCF8kGm7gDgtpVK18TCrcxGEivLSL6Nhd4",
+      authDomain: "manager-428fc.firebaseapp.com",
+      databaseURL: "https://manager-428fc.firebaseio.com",
+      projectId: "manager-428fc",
+      storageBucket: "manager-428fc.appspot.com",
+      messagingSenderId: "171496360954",
+      appId: "1:171496360954:web:9ff1d1937814844b"
+    };
+    firebase.initializeApp(firebaseConfig);
+   }
+  render() {
+    return (
+      <Provider store={createStore(reducers,{},applyMiddleware(ReduxThunk))}>
+     <LoginForm/>
+      </Provider>
+    
+    );
+  }
+}
+
+export default App;
