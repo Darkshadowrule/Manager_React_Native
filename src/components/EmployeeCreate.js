7affba779a63e900f813ab36e030b85f7b83fc15
darkshadowrule <prajjwaldxi3@gmail.com>
2019-05-19 22:33:48 +0530

    Added picker to select shift

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Picker,Text} from 'react-native';
 import {Card, CardSection, Input, Button} from './common';
 import {connect} from 'react-redux';
 import {employeeUpdate} from '../actions'
@@ -23,6 +24,22 @@ class EmployeeCreate extends React.Component{
                     onChangeText={text=>this.props.employeeUpdate({prop:'phone',value:text})}
                     />
                     </CardSection>
+                    <CardSection style={{flexDirection: 'column'}}>
+                        <Text style={styles.pickerTextStyle}>Shift</Text>
+                        <Picker
+                        style={{flex:1}}
+                        selectedValue={this.props.shift}
+                        onValueChange={day => this.props.employeeUpdate({prop:'shift',value:day})}>
+                        
+                            <Picker.Item label="Monday" value ="Monday" />
+                            <Picker.Item label="Tuesday" value ="Tuesday" />
+                            <Picker.Item label="Wednesday" value ="Wednesday" />
+                            <Picker.Item label="Thursday" value ="Thursday" />
+                            <Picker.Item label="Friday" value ="Friday" />
+                            <Picker.Item label="Saturday" value ="Saturday" />
+                            <Picker.Item label="Sunday" value ="Sunday" />
+                        </Picker>
+                    </CardSection>
                     <CardSection>
                     <Button>
                         Create
@@ -36,5 +53,10 @@ const mapStateToProps=(state) => {
     const {name,phone,shift}=state.employeeForm
     return {name,phone,shift}
 }
-
+const styles={
+    pickerTextStyle:{
+        fontSize:18,
+        paddingLeft: 20,
+    }
+}
 export default connect(mapStateToProps,{employeeUpdate})(EmployeeCreate) ;
